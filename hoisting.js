a8b7ca89094864f41bf12b521b0c3c953c36cccf
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-18 18:55:43 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -22,11 +22,11 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
+function sayMy() {
     console.log(name);
     var name = "Kristin";
+  sayMy();
+
+  
   }
 }
